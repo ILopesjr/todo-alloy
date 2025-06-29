42b75ad1248b96c6e8edc2120e44f71a03fc88fd
Ivanildo Lopes <ivanildo.lopes.jr@gmail.com>
2025-06-29 10:46:58 -0300

    Criando TaskList.vue

---
+++ resources/js/components/TaskList.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div v-if="loading">Carregando tarefas...</div>
+        <div v-if="error" class="text-red-500">{{ error }}</div>
+
+        <div v-for="task in tasks" :key="task.id">
+            <slot :task="task"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        tasks: {
+            type: Array,
+            required: true,
+            default: () => []
+        },
+        loading: {
+            type: Boolean,
+            default: false
+        },
+        error: {
+            type: String,
+            default: ''
+        }
+    }
+}
+</script>
