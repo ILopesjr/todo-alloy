2d4119f47320b715d1cbcb6d500b7b6acaff8022
Ivanildo Lopes <ivanildo.lopes.jr@gmail.com>
2025-06-29 10:47:09 -0300

    Criando TaskModal.vue

---
+++ resources/js/components/TaskModal.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
+        <div class="bg-white rounded-lg shadow-xl p-6 w-full max-w-md">
+            <h2 class="text-xl font-bold mb-4">{{ task ? 'Editar Tarefa' : 'Nova Tarefa' }}</h2>
+
+            <TaskForm :task="task" @save="onSave" @cancel="close" />
+
+        </div>
+    </div>
+</template>
+
+<script>
+import TaskForm from './TaskForm.vue';
+
+export default {
+    components: {
+        TaskForm
+    },
+    props: {
+        task: {
+            type: Object,
+            default: null
+        }
+    },
+    emits: ['close', 'save'],
+    methods: {
+        onSave(taskData) {
+            this.$emit('save', taskData);
+        },
+        close() {
+            this.$emit('close');
+        }
+    }
+}
+</script>
